f5c6ea91d3f0cde9687acf23062e8b882c3716e5
aedmark <a.edmark@gmail.com>
2026-07-10 14:01:35 -0500

    Delete bone_dev_2026 directory

--- bone_dev_2026/spores/spore_utils.py
+++
@@ -1,8 +0,0 @@
-"""spores/spore_utils.py"""
-
-import hashlib
-
-
-def _word_to_vector(word: str, dim: int = 8) -> list:
-    h = hashlib.shake_256(word.encode("utf-8")).digest(dim)
-    return [(b / 127.5) - 1.0 for b in h]
